719fbf3294ce5341b1f77f1159a2dec269554d90
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-02-03 21:03:33 -0800

    I've completed number 1 and am now moving on to number 2

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //1. Write a function that outputs a greeting. It should take an argument "name: and greet the individual//
-function myGreeting = (name) {
-    return "Howdy!";
-}
+const greetMe = (myName) => {
+    let myGreeting = "Howdy, ";
+    return myGreeting + myName + "!";
+};
+
+console.log(greetMe("Partner"));
